bf27d1b6bc1fd23ca5bc55e35b2bee7abc1874bb
Kezikov Boris <boris200898@icloud.com>
2019-05-08 12:34:24 +0300

    save updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,6 +10,9 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,44 +29,48 @@ public class Controller  implements Initializable {
     @FXML
     private MenuItem saveMenuItem;
 
-
     @FXML
     private HTMLEditor textEditor;
+
+
+
 
     @Override
     public void initialize (URL location, ResourceBundle resources){}
 
-//
-//    public void showDateTime(javafx.event.ActionEvent actionEvent) {
-//        System.out.println("Button clicked");
-//        Date now = new Date();
-//        DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
-//        String dateTimeString = df.format(now);
-//        myTextField.setText(dateTimeString);
-//    }
 
     public void createFile(ActionEvent actionEvent) {
-        Label secondLabel = new Label("I'm a Label on new Window");
-
-        StackPane secondaryLayout = new StackPane();
-        secondaryLayout.getChildren().add(secondLabel);
-
-        Scene secondScene = new Scene(secondaryLayout, 230, 100);
-
-        // New window (Stage)
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Second Stage");
-        newWindow.setScene(secondScene);
-
-        // Set position of second window, related to primary window.
-
-        newWindow.show();
+        System.out.println("kfkfkfkf");
 
     }
-
     public void openFile(ActionEvent actionEvent) {
+        System.out.println("File { } opened");
     }
 
     public void saveFile(ActionEvent actionEvent) {
+        System.out.println(System.getProperty("os.name"));
+//        FileOutputStream fos = new FileOutputStream("")
+
+
+
+
     }
 }
+
+
+
+
+
+//{Label secondLabel = new Label("I'm a Label on new Window");
+//    StackPane secondaryLayout = new StackPane();
+//        secondaryLayout.getChildren().add(secondLabel);
+//                Scene secondScene = new Scene(secondaryLayout, 230, 100);
+//
+//                // New window (Stage)
+//                Stage newWindow = new Stage();
+//                newWindow.setTitle("Second Stage");
+//                newWindow.setScene(secondScene);
+//
+//                // Set position of second window, related to primary window.
+//
+//                newWindow.show();}
