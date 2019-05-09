7e4e41cb64c35a6c7254f3a161093505110206c1
boriskezikov <boris200898>
2019-05-09 15:44:31 +0300

    Open file procedure implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,40 +2,20 @@ package sample;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Scanner;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Skin;
-import javafx.scene.control.SkinBase;
-import javafx.scene.control.Skinnable;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 
 public class Controller implements Initializable {
-
-    @FXML
-    private Button myButton;
-
-    @FXML
-    private MenuItem newFileMenuItem;
-
-    @FXML
-    private MenuItem openFileMenuItem;
-
-    @FXML
-    private MenuItem saveMenuItem;
-
 
     @FXML
     private TextArea textEditor;
@@ -49,14 +29,55 @@ public class Controller implements Initializable {
 
     }
 
-    public void openFile(ActionEvent actionEvent) {
-        System.out.println("File { } opened");
+    public void openFile(ActionEvent actionEvent) throws IOException {
+        final FileChooser fileChooser = new FileChooser();
+        textEditor.clear();
+        try
+        {
+            Scanner s = new Scanner(fileChooser.showOpenDialog(Main.getPrimaryStage()));
+            while (s.hasNext())
+            {
+                String line = s.nextLine();
+                if (line.contains("$"))
+                {
+                    String[] splitLine = line.split("\\s+");
+                    for (String entry : splitLine)
+                    {
+                        if (entry.contains("$"))
+                        {
+                            textEditor.appendText("\t\t\t\t " + entry + "\n");
+                        }
+                        else
+                        {
+                            textEditor.appendText(entry + " ");
+                        }
+                    }
+                }
+                else if (line.contains("-"))
+                {
+                    textEditor.appendText("\t\t\t\t\t\t" + line.trim() + "\n");
+                    System.out.println(line);
+                }
+                else if (line.contains("Amount"))
+                {
+                    String[] splitLine = line.split("\\s+");
+                    textEditor.appendText(splitLine[0] + "\t\t\t" + splitLine[1] + "\t\t" + splitLine[2] + "\n");
+                }
+                else
+                {
+
+                    textEditor.appendText(line + "\n");
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     public void saveFile(ActionEvent actionEvent) throws IOException {
-        System.out.println(textEditor.getSkin());
         String path = System.getProperty("user.dir");
-        System.out.println(path);
         FileWriter fos = new FileWriter(
             new File(path, "name.txt"));
         fos.write(textEditor.getText());
@@ -78,6 +99,7 @@ public class Controller implements Initializable {
         String os = System.getProperty("os.name").toLowerCase();
         return (os.contains("nix") || os.contains("nux"));
     }
+
 
 }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,9 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        setPrimaryStage(primaryStage);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("File Editor");
         primaryStage.setScene(new Scene(root, 600, 500));
@@ -19,5 +21,13 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void setPrimaryStage(Stage stage) {
+        Main.primaryStage = stage;
+    }
+
+     static Stage getPrimaryStage() {
+        return Main.primaryStage;
     }
 }
