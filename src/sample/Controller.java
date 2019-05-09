9a19d6b82ac9a3ca32a8f72f64b75db52d5709d1
boriskezikov <boris200898>
2019-05-09 15:10:59 +0300

    Html issue

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,29 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.StackPane;
-import javafx.scene.web.HTMLEditor;
-import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
+import javafx.scene.control.Skinnable;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.HTMLEditor;
 
-public class Controller  implements Initializable {
+public class Controller implements Initializable {
+
     @FXML
     private Button myButton;
 
@@ -32,34 +39,68 @@ public class Controller  implements Initializable {
     @FXML
     private HTMLEditor textEditor;
 
-
-
+    @FXML
+    private TextArea testText;
 
     @Override
-    public void initialize (URL location, ResourceBundle resources){}
-
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
     public void createFile(ActionEvent actionEvent) {
         System.out.println("kfkfkfkf");
 
     }
+
     public void openFile(ActionEvent actionEvent) {
         System.out.println("File { } opened");
     }
 
-    public void saveFile(ActionEvent actionEvent) {
-        System.out.println(System.getProperty("os.name"));
-//        FileOutputStream fos = new FileOutputStream("")
-
-
-
-
+    public void saveFile(ActionEvent actionEvent) throws IOException {
+        System.out.println(textEditor.getSkin());
+        String path = System.getProperty("user.dir");
+        System.out.println(path);
+        FileWriter fos = new FileWriter(
+            new File(path, "name.txt"));
+        fos.write(textEditor.getHtmlText());
+        fos.close();
     }
+
+    private static String getText(String htmlText) {
+
+        String result = "";
+
+        Pattern pattern = Pattern.compile("<[^>]*>");
+        Matcher matcher = pattern.matcher(htmlText);
+        final StringBuffer text = new StringBuffer(htmlText.length());
+
+        while (matcher.find()) {
+            matcher.appendReplacement(
+                text,
+                " ");
+        }
+
+        matcher.appendTail(text);
+
+        result = text.toString().trim();
+
+        return result;
+    }
+    private static boolean isWindows() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("win"));
+    }
+
+    private static boolean isMac() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("mac"));
+    }
+
+    private static boolean isUnix() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("nix") || os.contains("nux"));
+    }
+
 }
-
-
-
-
 
 //{Label secondLabel = new Label("I'm a Label on new Window");
 //    StackPane secondaryLayout = new StackPane();
