2be000563dae792b72a664b6162f1af6cc5d1c6c
boriskezikov <boris200898>
2019-05-09 19:14:11 +0300

    last

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,29 @@
 package sample;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 public class Controller implements Initializable {
 
     private final FileChooser fileChooser = new FileChooser();
 
+    private Path openedPath;
+
     @FXML
     private TextArea textEditor;
 
     @FXML
-    private TextField console;
+    private TextArea console;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -38,6 +38,8 @@ public class Controller implements Initializable {
         try {
             File file = fileChooser.showOpenDialog(Main.getPrimaryStage());
             if (file != null) {
+                openedPath = file.toPath();
+                System.out.println(openedPath);
                 Scanner s = new Scanner(file);
                 while (s.hasNext()) {
                     String line = s.nextLine();
@@ -46,32 +48,41 @@ public class Controller implements Initializable {
                 console.setText("\nFile opened: " + file.getAbsolutePath());
             }
 
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+        } catch (IOException ex) {
+            console.setText("Uuups! Something goes wrong! " + ex.getMessage());
         }
     }
 
     public void saveFile() throws IOException {
-         File file = fileChooser.showSaveDialog(Main.getPrimaryStage());
-        if (file != null){
-            if (!file.getName().contains(".")){
-                file = new File(file.getName() + ".txt");
-            }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(textEditor.getText());
-            console.setText("\nFile saved: " + file.getAbsolutePath());
-            return;
+        if (openedPath != null) {
+            Files.write(openedPath, textEditor.getText().getBytes());
+            console.setText("File saved");
         }
-        console.setText("\nFile save procedure has been aborted.");
-
+        else{
+            saveAsFile();
+        }
     }
 
-    @Deprecated
-    public void consoleLog(){
+    public void saveAsFile() {
+        try {
+            File file = fileChooser.showSaveDialog(Main.getPrimaryStage());
+            if (file != null) {
+                if (!file.getName().contains(".") || file.getName().indexOf(".") != -1) {
+                    file = new File(file.getName().replace(".", "") + ".txt");
+                }
+                openedPath = file.toPath();
+                Files.write(openedPath, textEditor.getText().getBytes());
+                console.setText("\nFile saved at path: " + file.getAbsolutePath());
+                return;
+            }
+            console.setText("\nFile save procedure has been aborted.");
 
+        } catch (IOException ex) {
+            console.setText("Uuups! Something goes wrong! " + ex.getMessage());
+        }
     }
 
-    private boolean testIdentity(File freshFile, File oldFile){
+    private boolean testIdentity(File freshFile, File oldFile) {
         return freshFile.equals(oldFile);
     }
 
@@ -96,5 +107,7 @@ public class Controller implements Initializable {
                                + "────╚═╝");
 
     }
+
+
 }
 
