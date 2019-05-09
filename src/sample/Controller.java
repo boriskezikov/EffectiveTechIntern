dc58c822dd829fcc9596bf1e1542edeb14cf8846
boriskezikov <boris200898>
2019-05-09 17:53:55 +0300

    Save and open  procedures implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,8 @@
 package sample;
 
-
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
@@ -13,93 +12,89 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 public class Controller implements Initializable {
 
+    private final FileChooser fileChooser = new FileChooser();
+
     @FXML
     private TextArea textEditor;
+
+    @FXML
+    private TextField console;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
-    public void createFile(ActionEvent actionEvent) {
-        System.out.println("kfkfkfkf");
-
+    public void createFile() {
+        console.setText("\nFile created: ");
     }
 
-    public void openFile(ActionEvent actionEvent) throws IOException {
-        final FileChooser fileChooser = new FileChooser();
+    public void openFile() {
         textEditor.clear();
-        try
-        {
-            Scanner s = new Scanner(fileChooser.showOpenDialog(Main.getPrimaryStage()));
-            while (s.hasNext())
-            {
-                String line = s.nextLine();
-                if (line.contains("$"))
-                {
-                    String[] splitLine = line.split("\\s+");
-                    for (String entry : splitLine)
-                    {
-                        if (entry.contains("$"))
-                        {
-                            textEditor.appendText("\t\t\t\t " + entry + "\n");
-                        }
-                        else
-                        {
-                            textEditor.appendText(entry + " ");
-                        }
-                    }
-                }
-                else if (line.contains("-"))
-                {
-                    textEditor.appendText("\t\t\t\t\t\t" + line.trim() + "\n");
-                    System.out.println(line);
-                }
-                else if (line.contains("Amount"))
-                {
-                    String[] splitLine = line.split("\\s+");
-                    textEditor.appendText(splitLine[0] + "\t\t\t" + splitLine[1] + "\t\t" + splitLine[2] + "\n");
-                }
-                else
-                {
-
+        try {
+            File file = fileChooser.showOpenDialog(Main.getPrimaryStage());
+            if (file != null) {
+                Scanner s = new Scanner(file);
+                while (s.hasNext()) {
+                    String line = s.nextLine();
                     textEditor.appendText(line + "\n");
                 }
+                console.setText("\nFile opened: " + file.getAbsolutePath());
             }
-        }
-        catch (FileNotFoundException ex)
-        {
+
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void saveFile(ActionEvent actionEvent) throws IOException {
-        String path = System.getProperty("user.dir");
-        FileWriter fos = new FileWriter(
-            new File(path, "name.txt"));
-        fos.write(textEditor.getText());
-        fos.close();
+    public void saveFile() throws IOException {
+         File file = fileChooser.showSaveDialog(Main.getPrimaryStage());
+        if (file != null){
+            if (!file.getName().contains(".")){
+                file = new File(file.getName() + ".txt");
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(textEditor.getText());
+            console.setText("\nFile saved: " + file.getAbsolutePath());
+            return;
+        }
+        console.setText("\nFile save procedure has been aborted.");
+
     }
 
+    @Deprecated
+    public void consoleLog(){
 
-    private static boolean isWindows() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return (os.contains("win"));
     }
 
-    private static boolean isMac() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return (os.contains("mac"));
+    private boolean testIdentity(File freshFile, File oldFile){
+        return freshFile.equals(oldFile);
     }
 
-    private static boolean isUnix() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return (os.contains("nix") || os.contains("nux"));
+    public void openLink(ActionEvent actionEvent) {
+        textEditor.setText("╔══╗╔══╦══╦╗╔╦╗╔╗\n"
+                               + "╚═╗║║╔═╣╔╗║║║║║║║\n"
+                               + "──║╚╝║─║║║║╚╝║╚╝║\n"
+                               + "──║╔╗║─║║║╠═╗╠═╗║\n"
+                               + "╔═╝║║╚═╣╚╝║─║║╔╝║\n"
+                               + "╚══╝╚══╩══╝─╚╝╚═╝\n"
+                               + "╔═══╦══╦═══╦══╦════╦══╦════╦╗\n"
+                               + "║╔═╗║╔╗║╔══╣╔╗╠═╗╔═╣╔╗╠═╗╔═╣║\n"
+                               + "║╚═╝║╚╝║╚══╣║║║─║║─║╚╝║─║║─║╚══╗\n"
+                               + "║╔══╣╔╗║╔═╗║║║║─║║─║╔╗║─║║─║╔═╗║\n"
+                               + "║║──║║║║╚═╝║╚╝║─║║─║║║║─║║─║╚═╝║\n"
+                               + "╚╝──╚╝╚╩═══╩══╝─╚╝─╚╝╚╝─╚╝─╚═══╝\n"
+                               + "────╔═╗\n"
+                               + "╔═══╬╗║\n"
+                               + "╚═══╝║║\n"
+                               + "╔═══╗║║\n"
+                               + "╚═══╬╝║\n"
+                               + "────╚═╝");
+
     }
-
-
 }
 
