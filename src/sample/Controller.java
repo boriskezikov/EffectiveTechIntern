973580394d7df4de0fb92e53c0a332c08940de26
boriskezikov <boris200898>
2019-05-10 13:55:20 +0300

    Shortcuts

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,37 +7,54 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
+
 
 
 public class Controller implements Initializable {
 
     private final FileChooser fileChooser = new FileChooser();
 
-    private static Path openedPath;
+    /** @value openedPath is a variable stores current work directory.(Opened file)*/
+    private  Path openedPath;
 
     @FXML
-    private TextArea textEditor;
+    private TextArea textEditor= new TextArea();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
+    /** Function which creates a new file */
     public void createFile() {
-        System.out.println("\nFile created: ");
+
+        /** @param alert Requests user confirmation to save file or not.*/
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+
+        alert.setTitle("Confirm action");
+        alert.setHeaderText("Do you want to save this document before closing?");
+        alert.setContentText(null);
+        alert.showAndWait().ifPresent(rs -> {
+            if (rs == ButtonType.OK) {
+                saveFile();}
+            openedPath = new File("new.txt").toPath();
+            setTitle();
+        });
+
     }
 
     public void openFile() {
-        textEditor.clear();
         try {
+            /** @param file  - Open dialog to choose any file*/
             File file = fileChooser.showOpenDialog(Main.getPrimaryStage());
             if (file != null) {
                 openedPath = file.toPath();
-                System.out.println(openedPath);
                 Scanner s = new Scanner(file);
                 while (s.hasNext()) {
                     String line = s.nextLine();
@@ -45,19 +62,24 @@ public class Controller implements Initializable {
                 }
                 System.out.println("\nFile opened: " + file.getAbsolutePath());
             }
-
+            setTitle();
         } catch (IOException ex) {
-            System.out.println("Uuups! Something goes wrong! " + ex.getMessage());
+            System.out.println("Uuups! Something goes wrong in 'openFile'! " + ex.getMessage());
         }
     }
 
-    public void saveFile() throws IOException {
-        if (openedPath != null) {
-            Files.write(openedPath, textEditor.getText().getBytes());
-            System.out.println("File saved");
-        }
-        else{
-            saveAsFile();
+    public void saveFile() {
+        try {
+            if (openedPath != null) {
+                Files.write(openedPath, textEditor.getText().getBytes());
+                System.out.println("File saved");
+            } else {
+                saveAsFile();
+            }
+            setTitle();
+
+        } catch (IOException ex) {
+            System.out.println("Uuups! Something goes wrong while saving file! " + ex.getMessage());
         }
     }
 
@@ -71,12 +93,12 @@ public class Controller implements Initializable {
                 openedPath = file.toPath();
                 Files.write(openedPath, textEditor.getText().getBytes());
                 System.out.println("\nFile saved at path: " + file.getAbsolutePath());
+                setTitle();
                 return;
             }
-            System.out.println("\nFile save procedure has been aborted.");
-
+            System.out.println("\nFile save procedure has been aborted. SAVE AS");
         } catch (IOException ex) {
-            System.out.println("Uuups! Something goes wrong! " + ex.getMessage());
+            System.out.println("Uuups! Something goes wrong saving AS! " + ex.getMessage());
         }
     }
 
@@ -87,12 +109,6 @@ public class Controller implements Initializable {
                                + "──║╔╗║─║║║╠═╗╠═╗║\n"
                                + "╔═╝║║╚═╣╚╝║─║║╔╝║\n"
                                + "╚══╝╚══╩══╝─╚╝╚═╝\n"
-                               + "╔═══╦══╦═══╦══╦════╦══╦════╦╗\n"
-                               + "║╔═╗║╔╗║╔══╣╔╗╠═╗╔═╣╔╗╠═╗╔═╣║\n"
-                               + "║╚═╝║╚╝║╚══╣║║║─║║─║╚╝║─║║─║╚══╗\n"
-                               + "║╔══╣╔╗║╔═╗║║║║─║║─║╔╗║─║║─║╔═╗║\n"
-                               + "║║──║║║║╚═╝║╚╝║─║║─║║║║─║║─║╚═╝║\n"
-                               + "╚╝──╚╝╚╩═══╩══╝─╚╝─╚╝╚╝─╚╝─╚═══╝\n"
                                + "────╔═╗\n"
                                + "╔═══╬╗║\n"
                                + "╚═══╝║║\n"
@@ -100,6 +116,16 @@ public class Controller implements Initializable {
                                + "╚═══╬╝║\n"
                                + "────╚═╝");
 
+    }
+
+    /** @exception java.lang.NullPointerException  */
+    private void setTitle(){
+        if (openedPath!=null){
+            Main.getPrimaryStage().setTitle(openedPath.getFileName().toString());
+        }
+        else{
+            Main.getPrimaryStage().setTitle("New file");
+        }
     }
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,9 +9,16 @@ import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
+/**
+ * Fx Notepad application.
+ *
+ * @version SNAPSHOT
+ */
+
 public class Main extends Application {
 
     private static Stage primaryStage;
+
     private Controller controller = new Controller();
 
     @Override
@@ -19,21 +26,22 @@ public class Main extends Application {
 
         setPrimaryStage(primaryStage);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("File Editor");
+        primaryStage.setTitle("Text editor");
         Scene scene = new Scene(root, 600, 500);
         primaryStage.setScene(scene);
-        scene.setOnKeyPressed(event -> {
-            System.out.println(event);
-            final KeyCombination kb = new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_ANY);
 
-            if (kb.match(event)) {
-                controller.saveAsFile();
+        scene.setOnKeyPressed(event -> {
+
+                                  /** @value kb - Key combination adds feature to save file using CTRL+S shortcut*/
+                                  final KeyCombination kb = new KeyCodeCombination(KeyCode.S,
+                                                                                   KeyCombination.CONTROL_ANY);
+
+                                  if (kb.match(event)) {
+                                      controller.saveFile();
                                   }
                               }
         );
         primaryStage.show();
-
-
     }
 
     public static void main(String[] args) {
